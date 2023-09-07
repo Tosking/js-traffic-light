e97a087c26d62c637880b83f2665d4dc19b0f26c
Tosking <tosking@duck.com>
2023-09-07 14:15:40 +0500

    added state display

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const carTraffic = document.querySelectorAll(".car-traffic__light")
 const manTraffic = document.querySelectorAll(".man-traffic__light")
 const timer = document.querySelector(".timer")
+const state = document.querySelector(".state")
 
 let it = -1
 
@@ -29,24 +30,28 @@ setInterval(() => {
     timer.innerHTML = it;
     console.log("123")
     if(it % 24 == 0){
+        state.innerHTML = "q1"
         carTraffic[0].style.backgroundColor = RED_d
         carTraffic[1].style.backgroundColor = YELLOW_d
         manTraffic[0].style.backgroundColor = RED
         carTraffic[2].style.backgroundColor = GREEN
     }
     else if(it % 24 == 10){
+        state.innerHTML = "q2"
         carTraffic[1].style.backgroundColor = YELLOW
         carTraffic[2].style.backgroundColor = GREEN_d
     }
     else if(it % 24 == 12){
+        state.innerHTML = "q3"
         manTraffic[0].style.backgroundColor = RED_d
         carTraffic[1].style.backgroundColor = YELLOW_d
         carTraffic[0].style.backgroundColor = RED
         manTraffic[1].style.backgroundColor = GREEN
     }
     else if(it % 24 == 22){
+        state.innerHTML = "q4"
         manTraffic[1].style.backgroundColor = GREEN_d
         manTraffic[0].style.backgroundColor = RED
         carTraffic[1].style.backgroundColor = YELLOW
     }
-}, 500)
+}, 1000)
